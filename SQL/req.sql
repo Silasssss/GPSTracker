afd8ae52938851d77914c5bcb7668c657e612855
Silasssss <silasdu22@gmail.com>
2018-03-19 15:51:24 +0100

    base de données

---
+++ SQL/req.sql
@@ -0,0 +1,41 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `confirmation_token` VARCHAR(60) NULL DEFAULT 0,
+  `confirmed_at` DATETIME NULL DEFAULT 0,
+  `reset_token` VARCHAR(60) NULL DEFAULT 0,
+  `reset_at` DATETIME NULL DEFAULT 0,
+  `remember_token` VARCHAR(250) NULL DEFAULT 0,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`coords`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`coords` (
+  `id` BIGINT NOT NULL,
+  `capteur_id` VARCHAR(255) NOT NULL,
+  `datetime` DATETIME NOT NULL,
+  `latitude` DOUBLE NOT NULL,
+  `longitude` DOUBLE NOT NULL,
+  `altitude` DOUBLE NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
